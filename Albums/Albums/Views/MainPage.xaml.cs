8172aec519cd5f67828f8d416d2755de7b1a5a34
Marco Von Ballmoos <marco@earthli.com>
2021-05-17 10:53:20 +0200

    Add initial Albums project

---
+++ Albums/Albums/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Albums.Views
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
