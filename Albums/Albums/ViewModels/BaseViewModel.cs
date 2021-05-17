5827b1b603946a9703f889f924662adb4fe65b6f
Marco von Ballmoos <marco@encodo.com>
2021-05-17 12:01:20 +0200

    Fix formatting in BaseViewModel.cs

--- Albums/Albums/ViewModels/BaseViewModel.cs
+++ Albums/Albums/ViewModels/BaseViewModel.cs
@@ -15,25 +15,27 @@ namespace Albums.ViewModels
 {
   public class BaseViewModel : INotifyPropertyChanged
   {
-    public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
-
-    bool isBusy = false;
     public bool IsBusy
     {
-      get { return isBusy; }
-      set { SetProperty(ref isBusy, value); }
+      get => _isBusy;
+      set => SetProperty(ref _isBusy, value);
     }
 
-    string title = string.Empty;
     public string Title
     {
-      get { return title; }
-      set { SetProperty(ref title, value); }
+      get => _title;
+      set => SetProperty(ref _title, value);
     }
 
-    protected bool SetProperty<T>(ref T backingStore, T value,
-        [CallerMemberName] string propertyName = "",
-        Action onChanged = null)
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+    protected bool SetProperty<T>(
+      ref T backingStore,
+      T value,
+      [CallerMemberName] string propertyName = "",
+      Action onChanged = null)
     {
       if (EqualityComparer<T>.Default.Equals(backingStore, value))
         return false;
@@ -44,8 +46,6 @@ namespace Albums.ViewModels
       return true;
     }
 
-    #region INotifyPropertyChanged
-    public event PropertyChangedEventHandler PropertyChanged;
     protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
     {
       var changed = PropertyChanged;
@@ -54,6 +54,8 @@ namespace Albums.ViewModels
 
       changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
-    #endregion
+
+    private bool _isBusy;
+    private string _title = string.Empty;
   }
 }
