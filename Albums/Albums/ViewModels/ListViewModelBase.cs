5e685e202d76637187c74ef2908617dfab77b99b
Marco Von Ballmoos <marco@earthli.com>
2021-05-17 12:42:09 +0200

    Add ListViewModelBase base class

---
+++ Albums/Albums/ViewModels/ListViewModelBase.cs
@@ -0,0 +1,58 @@
+﻿// <copyright file="ListViewModelBase.cs" company="Marco von Ballmoos">
+//   Copyright (c) 2021 Marco von Ballmoos. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Albums.Models;
+using Albums.Views;
+using Xamarin.Forms;
+
+namespace Albums.ViewModels
+{
+  public class ListViewModelBase : ViewModelBase
+  {
+    public ObservableCollection<Item> Items { get; set; }
+
+    public Command LoadItemsCommand { get; set; }
+
+    public ListViewModelBase()
+    {
+      Title = "Browse";
+      Items = new ObservableCollection<Item>();
+      LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+      MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+      {
+        var newItem = item as Item;
+        Items.Add(newItem);
+        await DataStore.AddItemAsync(newItem);
+      });
+    }
+
+    private async Task ExecuteLoadItemsCommand()
+    {
+      IsBusy = true;
+
+      try
+      {
+        Items.Clear();
+        var items = await DataStore.GetItemsAsync(true);
+        foreach (var item in items)
+        {
+          Items.Add(item);
+        }
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine(ex);
+      }
+      finally
+      {
+        IsBusy = false;
+      }
+    }
+  }
+}
