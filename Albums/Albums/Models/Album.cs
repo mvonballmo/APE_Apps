f9ad7576eada81db50695f2c4596b2991ddcafe8
Marco Von Ballmoos <marco@earthli.com>
2021-05-17 12:07:44 +0200

    Add model files

---
+++ Albums/Albums/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Album.cs" company="Marco von Ballmoos">
+//   Copyright (c) 2021 Marco von Ballmoos. All rights reserved.
+// </copyright>
+
+namespace Albums.Models
+{
+  // TODO Extract base class for common properties
+
+  public class Album
+  {
+    public string Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+  }
+}
---
+++ Albums/Albums/Models/Picture.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="Picture.cs" company="Marco von Ballmoos">
+//   Copyright (c) 2021 Marco von Ballmoos. All rights reserved.
+// </copyright>
+
+namespace Albums.Models
+{
+  public class Picture
+  {
+    public string Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string Url { get; set; }
+  }
+}
