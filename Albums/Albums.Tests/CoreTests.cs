9c66f3fb25d18d037b20b75b6fefe7e581f07291
Marco Von Ballmoos <marco@earthli.com>
2021-05-25 21:04:21 +0200

    Add more tests
    Review: Everyone

--- Albums/Albums.Tests/CoreTests.cs
+++ Albums/Albums.Tests/CoreTests.cs
@@ -8,6 +8,7 @@ using Albums.Models;
 using Albums.Services;
 using FakeItEasy;
 using NUnit.Framework;
+using SimpleInjector;
 
 namespace Albums.Tests
 {
@@ -17,20 +18,57 @@ namespace Albums.Tests
     [Test]
     public async Task TestSaveEmptyAlbum()
     {
-      var container = ContainerExtensions
-        .CreateContainer()
-        .RegisterAlbumServices();
-
-      var dialogService = A.Fake<IDialogService>();
-
-      container.RegisterInstance(dialogService);
-
+      var container = CreateContainer(out var dialogService);
       var albumSaver = container.GetInstance<IAlbumSaver>();
 
       var album = new Album();
 
       Assert.That(await albumSaver.TrySaveAsync(album), Is.False);
-      A.CallTo(() => dialogService.Show(A<string>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
+      A.CallTo(() => dialogService.Show("Validation failed", "The title cannot be empty.")).MustHaveHappenedOnceExactly();
+    }
+
+    [Test]
+    public async Task TestSaveAlbumWithNoDescription()
+    {
+      var container = CreateContainer(out var dialogService);
+      var albumSaver = container.GetInstance<IAlbumSaver>();
+
+      var album = new Album()
+      {
+        Title = "Something"
+      };
+
+      Assert.That(await albumSaver.TrySaveAsync(album), Is.False);
+      A.CallTo(() => dialogService.Show("Validation failed", "The description cannot be empty.")).MustHaveHappenedOnceExactly();
+    }
+
+    [Test]
+    public async Task TestSaveAlbumSucceeds()
+    {
+      var container = CreateContainer(out var dialogService);
+      var albumSaver = container.GetInstance<IAlbumSaver>();
+
+      var album = new Album()
+      {
+        Title = "Something",
+        Description = "Desc"
+      };
+
+      Assert.That(await albumSaver.TrySaveAsync(album), "Album should have been saved.");
+      A.CallTo(() => dialogService.Show(A<string>.Ignored, A<string>.Ignored)).MustNotHaveHappened();
+    }
+
+    private static Container CreateContainer(out IDialogService dialogService)
+    {
+      var container = ContainerExtensions
+        .CreateContainer()
+        .RegisterAlbumServices();
+
+      dialogService = A.Fake<IDialogService>();
+
+      container.RegisterInstance(dialogService);
+
+      return container;
     }
   }
 }
