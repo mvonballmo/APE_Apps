597c4737e0713d53d6785366794e245a4323c466
Marco Von Ballmoos <marco@earthli.com>
2021-06-28 20:40:59 +0200

    Improve ContentItem.ToString()

--- Albums/Albums/Models/ContentItem.cs
+++ Albums/Albums/Models/ContentItem.cs
@@ -9,5 +9,10 @@ namespace Albums.Models
     public string Title { get; set; }
 
     public string Description { get; set; }
+
+    public override string ToString()
+    {
+      return $"{Title}: {Description}";
+    }
   }
 }
