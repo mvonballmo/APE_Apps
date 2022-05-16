db77c5908ed0d4a6ee06d3e8ce7e82c98becfd3b
Marco von Ballmoos <marco@earthli.com>
2022-05-16 18:31:57 +0200

    Extract NotifyPropertyChangedViewModel

--- ToDo/ToDo/ViewModels/BaseViewModel.cs
+++ ToDo/ToDo/ViewModels/BaseViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
+﻿
 using Xamarin.Forms;
 
 using ToDo.Models;
@@ -10,7 +6,7 @@ using ToDo.Services;
 
 namespace ToDo.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : NotifyPropertyChangedViewModel
     {
         public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
 
@@ -27,30 +23,5 @@ namespace ToDo.ViewModels
             get { return title; }
             set { SetProperty(ref title, value); }
         }
-
-        protected bool SetProperty<T>(ref T backingStore, T value,
-            [CallerMemberName] string propertyName = "",
-            Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return false;
-
-            backingStore = value;
-            onChanged?.Invoke();
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            var changed = PropertyChanged;
-            if (changed == null)
-                return;
-
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        #endregion
     }
 }
