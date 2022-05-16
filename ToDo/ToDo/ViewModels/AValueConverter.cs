476e94d0a238c87f8ba9f2b483c4a1056589a354
Marco von Ballmoos <marco@earthli.com>
2022-05-16 19:32:43 +0200

    Add a converter example

---
+++ ToDo/ToDo/ViewModels/AValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ToDo.ViewModels
+{
+    public class AValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 198373636.ToString(culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
