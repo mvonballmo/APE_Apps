b81bced5b7b66de45bb1b99e2bdaa81848db9004
Marco von Ballmoos <marco@earthli.com>
2023-06-12 18:29:16 +0200

    Add FakeItEasy and implementation in CounterPersistenceTests.cs

--- Evaluation/Maui/Albums.Core.Tests/CounterPersistenceTests.cs
+++ Evaluation/Maui/Albums.Core.Tests/CounterPersistenceTests.cs
@@ -1,3 +1,4 @@
+using FakeItEasy;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 
@@ -6,55 +7,58 @@ namespace Albums.Core.Tests;
 public class CounterPersistenceTests : TestsBase
 {
   [Test]
-  public async Task TestAskUserToSave()
+  public async Task TestAskUserToSaveAndUserSaysYes()
   {
     var provider = CreateProvider();
     var service = provider.GetRequiredService<ICounterPersistence>();
     var state = provider.GetRequiredService<ICounterState>();
-    var dialogService = (TestDialogService)provider.GetRequiredService<IDialogService>();
+    var dialogService = provider.GetRequiredService<IDialogService>();
 
-    Assert.Multiple(() =>
-    {
-      Assert.That(dialogService.AskMessage, Is.EqualTo(string.Empty));
-      Assert.That(dialogService.ShowMessage, Is.EqualTo(string.Empty));
-    });
+    _flag = true;
 
     await service.Save(state);
 
     Assert.Multiple(() =>
     {
-      Assert.That(dialogService.AskMessage, Is.EqualTo("This is the alert"));
-      Assert.That(dialogService.ShowMessage, Is.EqualTo("Document saved."));
+      A.CallTo(() => dialogService.AskAsync("This is the alert")).MustHaveHappenedOnceExactly();
+      A.CallTo(() => dialogService.Show("Document saved.")).MustHaveHappenedOnceExactly();
+    });
+  }
+
+  [Test]
+  public async Task TestAskUserToSaveAndUserSaysNo()
+  {
+    var provider = CreateProvider();
+    var service = provider.GetRequiredService<ICounterPersistence>();
+    var state = provider.GetRequiredService<ICounterState>();
+    var dialogService = provider.GetRequiredService<IDialogService>();
+
+    _flag = false;
+
+    await service.Save(state);
+
+    Assert.Multiple(() =>
+    {
+      A.CallTo(() => dialogService.AskAsync("This is the alert")).MustHaveHappenedOnceExactly();
+      A.CallTo(() => dialogService.Show("Document NOT saved.")).MustHaveHappenedOnceExactly();
     });
   }
 
   protected override IServiceCollection AddServices(ServiceCollection serviceCollection)
   {
+    var fakeDialogService = A.Fake<IDialogService>();
+
+    A.CallTo(() => fakeDialogService.AskAsync("This is the alert")).ReturnsLazily(GetFlag);
+
     return base
         .AddServices(serviceCollection)
-        .AddSingleton<IDialogService, TestDialogService>();
+        .AddSingleton(fakeDialogService);
   }
 
-  public class TestDialogService : IDialogService
+  private bool _flag;
+
+  private bool GetFlag()
   {
-    public string AskMessage { get; set; } = string.Empty;
-
-    public string ShowMessage { get; set; } = string.Empty;
-
-    public Task<bool> AskAsync(string message)
-    {
-      AskMessage = message;
-
-      return Task.FromResult(true);
-    }
-
-    public Task Show(string message)
-    {
-      ShowMessage = message;
-
-      return Task.CompletedTask;
-    }
+    return _flag;
   }
-
-  // TODO FakeItEasy
 }
