9819c42e7a04fd51b4636dd603fa97b781341755
Marco von Ballmoos <marco@earthli.com>
2023-07-09 21:34:53 +0200

    Remove unused/non-compiling code

--- Maui/Albums/Albums/Platforms/Android/MainActivity.cs
+++ Maui/Albums/Albums/Platforms/Android/MainActivity.cs
@@ -8,21 +8,3 @@ namespace Albums;
 public class MainActivity : MauiAppCompatActivity
 {
 }
-
-public class NotificationService : INotificationService
-{
-  private const string ChannelId = "Channel ID";
-  private const string ChannelName = "TODO Channel";
-  private const string ChannelDescription = "Messages for the TODO App";
-
-  public void CreateNotificationChannel()
-  {
-    var channel = new NotificationChannel(ChannelId, ChannelName, NotificationImportance.Default)
-    {
-      Description = ChannelDescription
-    };
-
-    var notificationManager = (NotificationManager)MainActivity.Activity.GetSystemService(Context.NotificationService);
-    notificationManager.CreateNotificationChannel(channel);
-  }
-}
