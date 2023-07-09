9d5e707952f2254c751c57d94578551c0ce7601c
Marco von Ballmoos <marco@earthli.com>
2023-07-09 21:35:22 +0200

    Copy in code for communicating with a server

---
+++ Maui/Albums/Albums.Core/Part.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsClient.Data
+{
+    [Serializable]
+    public class Part : INotifyPropertyChanged
+    {
+        string _partId;
+        public string PartID
+        {
+            get => _partId;
+            set
+            {
+                if (_partId == value)
+                    return;
+
+                _partId = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PartID)));
+            }
+        }
+
+        string _partName;
+        public string PartName
+        {
+            get => _partName;
+            set
+            {
+                if (_partName == value)
+                    return;
+
+                _partName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PartName)));
+            }
+        }
+
+        string _theSuppliers;
+        public string TheSuppliers
+        {
+            get => _theSuppliers;
+            set
+            {
+                if (_theSuppliers == value)
+                    return;
+
+                _theSuppliers = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TheSuppliers)));
+            }
+        }
+
+        string _partType;
+        public string PartType
+        {
+            get => _partType;
+            set
+            {
+                if (_partType == value)
+                    return;
+
+                _partType = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PartType)));
+            }
+        }
+
+        public List<string> Suppliers { get; set; }
+        public DateTime PartAvailableDate { get; set; }
+
+        public string SupplierString
+        {
+            get
+            {
+                string result = String.Empty;
+                foreach (string supplier in Suppliers)
+                {
+                    result += $"{supplier}, ";
+                }
+                result = result.Trim(',', ' ');
+                return result;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Maui/Albums/Albums.Core/PartsManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace PartsClient.Data
+{
+    public static class PartsManager
+    {
+        static readonly string BaseAddress = "https://localhost:3000";
+        static readonly string Url = $"{BaseAddress}/api/";
+        private static string authorizationKey;
+
+        static HttpClient client;
+
+        private static async Task<HttpClient> GetClient()
+        {
+            if (client != null)
+                return client;
+
+            client = new HttpClient();
+
+            if (string.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await client.GetStringAsync($"{Url}login");
+                authorizationKey = JsonConvert.DeserializeObject<string>(authorizationKey);
+            }
+
+            client.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
+        }
+
+        public static async Task<IEnumerable<Part>> GetAll()
+        {
+            return new List<Part>()
+            {
+                new Part
+                {
+                     PartName = "CPU"
+                },
+                new Part
+                {
+                    PartName = "RAM"
+                }
+            };
+        }
+
+        public static async Task<Part> Add(string partName, string supplier, string partType)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return new Part();
+
+            Part part = new Part()
+            {
+                PartName = partName,
+                Suppliers = new List<string>(new[] { supplier }),
+                PartID = string.Empty,
+                PartType = partType,
+                PartAvailableDate = DateTime.Now.Date
+            };
+
+            var msg = new HttpRequestMessage(HttpMethod.Post, $"{Url}parts");
+
+            msg.Content = JsonContent.Create<Part>(part);
+
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
+
+            var returnedJson = await response.Content.ReadAsStringAsync();
+
+            var insertedPart = JsonConvert.DeserializeObject<Part>(returnedJson);
+
+            return insertedPart;
+        }
+
+        public static async Task Update(Part part)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return;
+
+            HttpRequestMessage msg = new(HttpMethod.Put, $"{Url}parts/{part.PartID}");
+            msg.Content = JsonContent.Create<Part>(part);
+            HttpClient client = await GetClient();
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task Delete(string partID)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return;
+            HttpRequestMessage msg = new(HttpMethod.Delete, $"{Url}parts/{partID}");
+            HttpClient client = await GetClient();
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
